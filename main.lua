67e648f05a388b82d832687055305a426dcda770
Tyler Neylon <tylerneylon@gmail.com>
2015-10-14 17:10:37 -0700

    Build the graph data structure G.

--- main.lua
+++ main.lua
@@ -13,6 +13,9 @@ local entities = {}
 
 local tile_size = 35
 
+-- G[x][y] = {nbors of that grid pt}.
+local G = {}
+
 
 -- Internal functions.
 
@@ -30,6 +33,50 @@ function draw_dot(x, y, r)
                        (x + 0.5) * tile_size, (y + 0.5) * tile_size, r, 20)
 end
 
+function draw_path(path)
+  local p = {}
+  for _, coord in ipairs(path) do
+    p[#p + 1] = (coord + 0.5) * tile_size
+  end
+  love.graphics.line(p)
+end
+
+-- This builds the graph structure in G.
+-- G[x][y] = {nbors of grid point (x, y)}
+function build_G()
+  for x = 1, x_len do G[x] = {} end
+  for y = 1, y_len do
+    for x = 1, x_len do
+      if maze[x][y] == 0 then  -- Make sure the grid space is not a wall.
+        G[x][y] = {}  -- This is the nbor list.
+        if maze[x + 1][y] == 0 then table.insert(G[x][y], {x + 1, y}) end
+        if maze[x - 1][y] == 0 then table.insert(G[x][y], {x - 1, y}) end
+        if maze[x][y + 1] == 0 then table.insert(G[x][y], {x, y + 1}) end
+        if maze[x][y - 1] == 0 then table.insert(G[x][y], {x, y - 1}) end
+      end
+    end
+  end
+
+  -- Below is debug code to help verify that G is correct.
+  --[[
+  for y = 1, y_len do
+    for x = 1, x_len do
+      if G[x][y] then
+        pr('G[%d][%d] has nbors:', x, y)
+        for _, nbor in pairs(G[x][y]) do
+          pr('  {%d, %d}', nbor[1], nbor[2])
+        end
+      end
+    end
+  end
+  --]]
+end
+
+-- This returns a list of x, y pairs starting at the given arguments and
+-- ending at the goal point x_len, 1.
+function find_path(x, y)
+  return {x, y, x + 1, y}
+end
 
 -- Entity class.
 
@@ -38,12 +85,16 @@ local Entity = {}
 Entity.__index = Entity
 
 function Entity:draw()
+  love.graphics.setColor(100, 0, 120)
+  draw_path(self.path)
+
   love.graphics.setColor(190, 0, 220)
   draw_dot(self.x, self.y, 7)
 end
 
 function Entity.new(x, y)
   local entity = {x = x, y = y}
+  entity.path = find_path(x, y)
   return setmetatable(entity, Entity)
 end
 
@@ -62,7 +113,7 @@ function love.load()
   for line in maze_str:gmatch('[^\n]+') do
     if x_len == nil then
       x_len = #line
-      for x = 1, x_len do maze[x] = {} end
+      for x = 0, x_len + 1 do maze[x] = {} end
     end
     x = 1
     for c in line:gmatch('.') do
@@ -72,6 +123,9 @@ function love.load()
     y = y + 1
   end
   y_len = #maze[1]
+
+  -- Build the grid connectivity graph.
+  build_G()
 
   -- Below is debug code to check that the maze was parsed correctly.
   --[[
